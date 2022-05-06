0294f1f5bcc7d7e35ea97d4cd9095430aebf2cd5
SonJaeGon <wprhs0650@gmail.com>
2022-05-06 13:33:30 +0900

    Algorithm yeardream

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 print('hello')
 
-for i in range(1,20):
-    if i % 3 == 0:
+for i in range(1,20+1):
+    if i % 15 == 0:
+        print("yeardream")
+    elif i % 3 == 0:
         print("year")
     elif i % 5 == 0:
         print("dream")
-    elif i % 15 == 0:
-        print("yeardream")
     else:
         print(i)
