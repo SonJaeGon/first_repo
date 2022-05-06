6ee29dc83df0143e5b462709d4f863897a819ba0
SonJaeGon <wprhs0650@gmail.com>
2022-05-06 13:20:42 +0900

    5, 15배수문제
    print yeardream(15)

--- hello.py
+++ hello.py
@@ -3,5 +3,9 @@ print('hello')
 for i in range(1,10):
     if i % 3 == 0:
         print("year")
+    elif i % 5 == 0:
+        print("dream")
+    elif i % 15 == 0:
+        print("yeardream")
     else:
         print(i)
